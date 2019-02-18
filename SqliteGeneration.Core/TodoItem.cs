9f6ed72f1327ca73f5b3c69d8193825f6cc12d46
Daniel John Causer <DanielCauser@users.noreply.github.com>
2019-02-18 12:19:43 -0500

    [DC] Model change

--- SqliteGeneration.Core/TodoItem.cs
+++ SqliteGeneration.Core/TodoItem.cs
@@ -6,7 +6,7 @@ namespace SqliteGeneration.Core
     public class TodoItem
     {
         [PrimaryKey]
-        public string Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
     }
 }
